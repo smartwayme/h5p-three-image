baa488f6c48194d97f3784f86bf803d13b243009
thomasmars <thomasmars87@gmail.com>
2018-05-04 14:00:43 +0200

    HFP-1995 Polished prototype

--- scripts/three-image.js
+++ scripts/three-image.js
@@ -29,57 +29,145 @@ H5P.ThreeImage = (function (EventDispatcher, ThreeSixty) {
       wrapper.classList.add('h5p-three-sixty-wrapper');
 
       // Create source image
-      initSceneFromImage(H5P.getPath(parameters.file.path, contentId), function (scene) {
-        var firstSceneNavButton1 = document.createElement('div');
-        firstSceneNavButton1.classList.add('nav-button');
-        firstSceneNavButton1.innerText = '+';
-        firstSceneNavButton1.addEventListener('click', function () {goToScene(self.secondScene)});
-        scene.add(firstSceneNavButton1, {yaw: -5.290342653589794, pitch: 0.25875}, false);
+      initSceneFromImage(H5P.getPath(parameters.sphere.path, contentId), function (scene) {
+        addNavButtonToScene(scene, -5.290342653589794, 0.25875, function () {
+          goToScene(self.secondScene);
+        });
+
+        addNavButtonToScene(scene, -2.1340926535897933, 0.375, function () {
+          goToScene(self.fourthScene);
+        });
+        addImageButtonToScene(scene, -10.055342653589792, -0.21625, parameters.sphere1image1.path);
 
         // Initial setup of scenes
-        var firstScene = scene;
-        wrapper.appendChild(firstScene.element);
+        wrapper.appendChild(scene.element);
         scene.resize();
         threeSixty = scene;
         threeSixty.startRendering();
         self.firstScene = scene;
       });
 
-      initSceneFromImage(H5P.getPath(parameters.file2.path, contentId), function (scene) {
-        var secondScene = scene;
-
-        var secondSceneNavButton1 = document.createElement('div');
-        secondSceneNavButton1.classList.add('nav-button');
-        secondSceneNavButton1.innerText = '+';
-        secondSceneNavButton1.addEventListener('click', function () {goToScene(self.firstScene)});
-        secondScene.add(secondSceneNavButton1, {yaw: -5.254092653589793, pitch: -0.39875}, false);
-
-        var secondSceneImageButton = document.createElement('div');
-        secondSceneImageButton.classList.add('image-button');
-        secondSceneImageButton.innerText = '+';
-        secondSceneImageButton.addEventListener('click', function () {
-          var img = document.createElement('img');
-          img.classList.add('h5p-image-popup');
-          img.src = H5P.getPath(parameters.file2image1.path, contentId);
-          wrapper.appendChild(img);
-
-          var closeButton = document.createElement('div');
-          closeButton.classList.add('h5p-close-button');
-          closeButton.innerText = 'X';
-          wrapper.appendChild(closeButton);
-
-          closeButton.addEventListener('click', function () {
-            wrapper.removeChild(img);
-            wrapper.removeChild(closeButton);
-          });
+      initSceneFromImage(H5P.getPath(parameters.sphere2.path, contentId), function (scene) {
+        addNavButtonToScene(scene, -5.254092653589793, -0.39875, function () {
+          goToScene(self.firstScene);
         });
-        secondScene.add(secondSceneImageButton, {yaw: -8.456592653589793, pitch: -0.04}, false);
 
-        self.secondScene = secondScene;
+        addNavButtonToScene(scene, -5.266592653589793, -0.71625, function () {
+          goToScene(self.thirdScene);
+        });
+        addImageButtonToScene(scene, -8.382842653589792, 1.46375, parameters.sphere2image1.path);
+        addImageButtonToScene(scene, -8.416592653589793, 1.12, parameters.sphere2image2.path);
+        addImageButtonToScene(scene, -8.456592653589793, -0.04, parameters.sphere2image3.path);
+        self.secondScene = scene;
       });
-      initSceneFromImage(H5P.getPath(parameters.file3.path, contentId), function (scene) {
+
+      initSceneFromImage(H5P.getPath(parameters.sphere3.path, contentId), function (scene) {
+        addNavButtonToScene(scene, -2.115342653589793, 0.42, function () {
+          goToScene(self.secondScene);
+        });
+        addImageButtonToScene(scene, -8.525342653589792, 0.32, parameters.sphere3image1.path);
+        addImageButtonToScene(scene, -8.322842653589792, -0.7275, parameters.sphere3image2.path);
+        addImageButtonToScene(scene, -8.256592653589793, -0.4625, parameters.sphere3image3.path);
         self.thirdScene = scene;
       });
+
+      initSceneFromImage(H5P.getPath(parameters.sphere4.path, contentId), function (scene) {
+        addNavButtonToScene(scene, -5.2740926535897925, -0.57125, function () {
+          goToScene(self.firstScene);
+        });
+
+        self.fourthScene = scene;
+      });
+    };
+
+    var addImageButtonToScene = function (scene, yaw, pitch, imagePath) {
+      var navButtonWrapper = document.createElement('div');
+      navButtonWrapper.classList.add('nav-button-wrapper');
+
+      var outerNavButton = document.createElement('div');
+      outerNavButton.classList.add('outer-nav-button');
+      navButtonWrapper.appendChild(outerNavButton);
+
+      var navButton = document.createElement('div');
+      navButton.classList.add('nav-button');
+      navButtonWrapper.appendChild(navButton);
+
+      var navButtonIcon = document.createElement('img');
+      navButtonIcon.src = self.getLibraryFilePath('assets/image.svg');
+      navButtonIcon.classList.add('nav-button-icon');
+      navButton.appendChild(navButtonIcon);
+
+      var navButtonPulsar = document.createElement('div');
+      navButtonPulsar.classList.add('nav-button-pulsar');
+      navButtonPulsar.addEventListener('click', function () {
+        var popup = document.createElement('div');
+        popup.classList.add('h5p-image-popup');
+
+        var img = document.createElement('img');
+        img.classList.add('h5p-image');
+        img.src = H5P.getPath(imagePath, contentId);
+        popup.appendChild(img);
+
+        addCloseButton(popup);
+
+        wrapper.appendChild(popup);
+      });
+      navButtonWrapper.appendChild(navButtonPulsar);
+
+      scene.add(navButtonWrapper, {yaw: yaw, pitch: pitch}, false);
+    };
+
+    var addNavButtonToScene = function (scene, yaw, pitch, callback) {
+      var navButtonWrapper = document.createElement('div');
+      navButtonWrapper.classList.add('nav-button-wrapper');
+
+      var outerNavButton = document.createElement('div');
+      outerNavButton.classList.add('outer-nav-button');
+      navButtonWrapper.appendChild(outerNavButton);
+
+      var navButton = document.createElement('div');
+      navButton.classList.add('nav-button');
+      navButtonWrapper.appendChild(navButton);
+
+      var navButtonIcon = document.createElement('img');
+      navButtonIcon.src = self.getLibraryFilePath('assets/navigation.svg');
+      navButtonIcon.classList.add('nav-button-icon');
+      navButton.appendChild(navButtonIcon);
+
+      var navButtonPulsar = document.createElement('div');
+      navButtonPulsar.classList.add('nav-button-pulsar');
+      navButtonPulsar.addEventListener('click', function () {callback()});
+      navButtonWrapper.appendChild(navButtonPulsar);
+
+      scene.add(navButtonWrapper, {yaw: yaw, pitch: pitch}, false);
+    };
+
+    var addCloseButton = function (popup) {
+      var navButtonWrapper = document.createElement('div');
+      navButtonWrapper.classList.add('nav-button-wrapper');
+
+      var outerNavButton = document.createElement('div');
+      outerNavButton.classList.add('outer-nav-button');
+      navButtonWrapper.appendChild(outerNavButton);
+
+      var navButton = document.createElement('div');
+      navButton.classList.add('nav-button');
+      navButtonWrapper.appendChild(navButton);
+
+      var navButtonIcon = document.createElement('img');
+      navButtonIcon.src = self.getLibraryFilePath('assets/navigation.svg');
+      navButtonIcon.classList.add('nav-button-icon');
+      navButton.appendChild(navButtonIcon);
+
+      var navButtonPulsar = document.createElement('div');
+      navButtonPulsar.classList.add('nav-button-pulsar');
+      navButtonPulsar.addEventListener('click', function () {
+        wrapper.removeChild(popup);
+      });
+      navButtonWrapper.appendChild(navButtonPulsar);
+
+      navButtonWrapper.classList.add('h5p-close-button');
+      popup.appendChild(navButtonWrapper);
     };
 
     var goToScene = function (scene) {
